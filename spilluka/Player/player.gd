92866da2e9bc02615702fa81819946f7e8fef897
niwaa009 <niwaa009@osloskolen.no>
2024-11-13 10:18:34 +0100

    .

--- spilluka/Player/player.gd
+++ spilluka/Player/player.gd
@@ -1,9 +1,18 @@
 extends CharacterBody2D
 
+<<<<<<< HEAD
 
 const speed = 300.0
 const jumpVelocity = -400.0
 
+=======
+const speed = 100.0
+const jumpVelocity = -225.0
+const customGravity = 700.0
+const maxJumps = 2
+
+var jumpCount = 0
+>>>>>>> d3bce8c (.)
 
 func _physics_process(delta: float) -> void:
 
@@ -12,13 +21,23 @@ func _physics_process(delta: float) -> void:
 	
 
 
+<<<<<<< HEAD
 func jump(delta):
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+=======
+func jump(delta): 
+	if not is_on_floor():
+		velocity.y += customGravity * delta
+	else:
+		jumpCount = 0
+>>>>>>> d3bce8c (.)
 
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if Input.is_action_just_pressed("up") and jumpCount < maxJumps:
 		velocity.y = jumpVelocity
+		jumpCount += 1
+		$jumpSound.play()  
 
 
 func movement():
@@ -26,6 +45,7 @@ func movement():
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * speed
+		$animation.flip_h = direction > 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
